90f2ca34cfd5882cd841ee087cafa4b2f2d19a70
Teerasej <teerasej@gmail.com>
2018-05-03 16:01:46 +0700

    นำ bootstrap มาใช้ตกแต่งใน web form

---
+++ application/views/fishing-vessel/new-ship.php
@@ -0,0 +1,17 @@
+<div class="row">
+    <div class="col-4"></div>
+    <div class="card col-4">
+        <div class="card-body">
+            <h5 class="card-title">ข้อมูลเรือประมง</h5>
+            <form action="">
+                <div class="form-group">
+                    <label for="">ชื่อ</label>
+                    <input class="form-control" type="text" >
+                </div>
+                <button class="btn btn-primary btn-lg">เพิ่ม</button>
+                <button class="btn btn-secondary btn-lg">ล้าง</button>
+            </form>
+        </div>
+    </div>
+    <div class="col-4"></div>
+</div>
