c9a3cbbbebb47a4fcdfae996da05db6426df64af
Teerasej <teerasej@gmail.com>
2018-05-03 14:06:54 +0700

    สร้าง controller สำหรับจัดการข้อมูลเรือประมง

---
+++ application/controllers/FishingVessel.php
@@ -0,0 +1,25 @@
+<?php
+
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class FishingVessel extends CI_Controller {
+
+    public function index()
+    {
+        echo 'Fishing Vessel';
+    }
+
+    public function all()
+    {
+        echo 'Show all vessels...';
+    }
+
+
+
+}
+
+/* End of file FishingVessel.php */
+
+
+?>
