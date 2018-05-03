f78219d56edc116dc3c7b537e95bc2e7d1b15add
Teerasej <teerasej@gmail.com>
2018-05-03 16:01:33 +0700

    แสดงการแบ่ง column ใน bootstrap

---
+++ application/views/fishing-vessel/index.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: blue"></div>
+    <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fisherman.</h1>
+    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: green"></div>
+</div>
