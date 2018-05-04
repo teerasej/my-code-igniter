8824d87831d438ff1d72b9a857581dd0a1646dfd
Teerasej <teerasej@gmail.com>
2018-05-04 14:09:51 +0700

    เอาข้อมูลจำลองออก และวนลูปแสดงข้อมูลเรือประมงจริง

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -1,46 +1,21 @@
 <div class="row">
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 ">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
+    <?php 
+        foreach ($vessels as $ship): 
+    ?>
     <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
         <div class="card">
             <div class="card-header">
-                ชื่อเรือ
+                <?php echo $ship['Name'] ?>
             </div>
             <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
+                <h5 class="card-title">
+                    <?php echo $ship['Country_ID'] ?>
+                </h5>
                 <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
             </div>
         </div>
     </div>
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
-    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-            <div class="card-header">
-                ชื่อเรือ
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">ประเทศ</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-    </div>
+    <?php
+        endforeach
+    ?>
 </div>
