d7a505fc5b5f87293b6745ad8d144086504256d3
Teerasej <teerasej@gmail.com>
2018-05-04 10:23:49 +0700

    เพิ่มตัวอย่าง card ข้อมูลเรือ และกำหนดขนาดให้เหมาะกับ Responsive

--- application/views/fishing-vessel/index.php
+++ application/views/fishing-vessel/index.php
@@ -1,5 +1,46 @@
 <div class="row">
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: blue"></div>
-    <h1 class="col-lg-4 col-sm-12 col-md-10">Hello, Fisherman.</h1>
-    <div class="col-lg-4 col-sm-12 col-md-1" style="background-color: green"></div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 ">
+        <div class="card">
+            <div class="card-header">
+                ชื่อเรือ
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">ประเทศ</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                ชื่อเรือ
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">ประเทศ</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                ชื่อเรือ
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">ประเทศ</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12">
+        <div class="card">
+            <div class="card-header">
+                ชื่อเรือ
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">ประเทศ</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            </div>
+        </div>
+    </div>
 </div>
