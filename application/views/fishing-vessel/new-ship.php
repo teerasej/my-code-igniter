ea158f4c0ac72c26b09f46ec8627389affa28e87
Teerasej <teerasej@gmail.com>
2018-05-04 14:10:33 +0700

    จัด layout บนหน้าจอ Responsive ใหม่

--- application/views/fishing-vessel/new-ship.php
+++ application/views/fishing-vessel/new-ship.php
@@ -1,6 +1,6 @@
 <div class="row">
-    <div class="col-4"></div>
-    <div class="card col-4">
+    <div class="col-lg-3 col-md-1 col-sm-0"></div>
+    <div class="card col-lg-6 col-md-10 col-sm-12">
         <div class="card-body">
             <h5 class="card-title">ข้อมูลเรือประมง</h5>
             <form action="">
@@ -13,5 +13,5 @@
             </form>
         </div>
     </div>
-    <div class="col-4"></div>
+    <div class="col-lg-3 col-md-1 col-sm-0"></div>
 </div>
