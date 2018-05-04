6b6a3aab9d6a221f2e387e79ba6c3b050c670f58
Teerasej <teerasej@gmail.com>
2018-05-04 10:07:47 +0700

    เพิ่ม Nav bar menu เข้าไปในส่วนหัวเว็บ

--- application/views/header.php
+++ application/views/header.php
@@ -8,5 +8,22 @@
     <title>Fishing Vessel</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Fishing Vessel Manager</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Dashboard<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">New Vessel</a>
+      </li>
+    </ul>
+  </div>
+</nav>
     <div class="container">
     
