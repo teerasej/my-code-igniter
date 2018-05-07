31b79b195c3779960d775f6ddb05926e7441faf2
Teerasej <teerasej@gmail.com>
2018-05-07 10:33:17 +0700

    เพิ่มส่วน validate ข้อมูลเรือก่อนลง database โดยกำหนดให้กรอกชื่อเรือ ไม่เกิน 10 ตัวอักษร

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -1,9 +1,9 @@
 <?php
 
+defined('BASEPATH') or exit('No direct script access allowed');
 
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class FishingVessel extends CI_Controller {
+class FishingVessel extends CI_Controller
+{
 
     public function index()
     {
@@ -20,7 +20,7 @@ class FishingVessel extends CI_Controller {
 
     public function all()
     {
-        echo 'Show all vessels...'; 
+        echo 'Show all vessels...';
     }
 
     public function new_vessel()
@@ -38,10 +38,29 @@ class FishingVessel extends CI_Controller {
 
     public function create()
     {
-        $this->load->model('fishingvessel_model');
-        $this->fishingvessel_model->save_new_vessel();
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('vesselName', 'ชื่อเรือ', 'required|max_length[10]', array('required' => 'ช่วยกรอกชื่อสำเภาด้วยนะออเจ้า' ));
 
-        redirect('fishingvessel/');
+        if ($this->form_validation->run() == false) 
+        {
+            $this->load->model('country_model');
+            $result = $this->country_model->get_all();
+
+            $data['country_list'] = $result;
+            $data['title'] = "เพิ่มข้อมูลเรือประมง";
+
+            $this->load->view('header', $data);
+            $this->load->view('fishing-vessel/new-ship', $data);
+            $this->load->view('footer');
+        }
+        else 
+        {
+            $this->load->model('fishingvessel_model');
+            $this->fishingvessel_model->save_new_vessel();
+
+            redirect('fishingvessel/');
+        }
+        
     }
 
     public function new_success()
@@ -49,11 +68,6 @@ class FishingVessel extends CI_Controller {
 
     }
 
-
-
 }
 
 /* End of file FishingVessel.php */
-
-
-?>
