f5836f8fd1803e02e29f6c830de6274ea72b44c9
Teerasej <teerasej@gmail.com>
2018-05-07 16:11:10 +0700

    แสดง chart ตรงจุดที่กำหนดชื่อเอาไว้ ด้วยข้อมูลจำลอง

--- assets/js/my-chart.js
+++ assets/js/my-chart.js
@@ -1,6 +1,33 @@
 
 $(document).ready(function(){
 
-    alert('ok');
+    $('#bar-chart').insertFusionCharts({
+        type: 'column2d',
+        width: "100%",
+        height: "400",
+        dataFormat: "json",
+        dataSource: {
+            "chart":{
+                "caption": "จำนวนเรือประมงแบ่งตามประเทศ",
+                "xAxisName": "ประเทศ",
+                "yAxisName": "จำนวน/ลำ",
+                "theme": "fint"
+            },
+            "data": [
+                {
+                    "label": "ประเทศไทย",
+                    "value": "120"
+                },
+                {
+                    "label": "บรูไน",
+                    "value": "230"
+                },
+                {
+                    "label": "ญี่ปุ่น",
+                    "value": "570"
+                }
+            ]
+        }
+    });
 
 });
