2ebc2e9606fdf81fdb89e7b84a3695c386cba2c7
Teerasej <teerasej@gmail.com>
2018-05-07 14:24:12 +0700

    1. ตั้งค่าสำหรับระบบ upload 2. สั่งอัพโหลดไฟล์หลังจาก validate form ผ่านแล้ว 3. ดึงชื่อ นามสกุลไฟล์ เอามาสร้างเป็นที่อยู่ไฟล์บนเว็บ

--- application/controllers/FishingVessel.php
+++ application/controllers/FishingVessel.php
@@ -38,8 +38,11 @@ class FishingVessel extends CI_Controller
 
     public function create()
     {
-
-        // เดี๋ยวจะใส่โค้ดอัพไฟล์ตรงนี้นะ
+        $config['upload_path'] = './upload/';
+        $config['allowed_types'] = 'gif|jpg|png|jpeg';
+        // $config['max_size'] = 100;
+        // $config['max_width'] = 1024;
+        $this->load->library('upload', $config);
 
         $this->load->library('form_validation');
         $this->form_validation->set_rules('vesselName', 'ชื่อเรือ', 'required|max_length[10]', array('required' => 'ช่วยกรอกชื่อสำเภาด้วยนะออเจ้า' ));
@@ -58,10 +61,34 @@ class FishingVessel extends CI_Controller
         }
         else 
         {
-            $this->load->model('fishingvessel_model');
-            $this->fishingvessel_model->save_new_vessel();
+            $upload_success = $this->upload->do_upload('vesselImage');
 
-            redirect('fishingvessel/');
+
+            if($upload_success)
+            {
+
+                $upload_data = $this->upload->data();
+                // var_dump($upload_data);
+
+                $image_path = 'upload/' . $upload_data['file_name'];
+
+                // $this->load->model('fishingvessel_model');
+                // $this->fishingvessel_model->save_new_vessel();
+
+                // redirect('fishingvessel/');
+            }
+            else 
+            {
+                $this->load->model('country_model');
+                $result = $this->country_model->get_all();
+
+                $data['country_list'] = $result;
+                $data['title'] = "เพิ่มข้อมูลเรือประมง";
+
+                $this->load->view('header', $data);
+                $this->load->view('fishing-vessel/new-ship', $data);
+                $this->load->view('footer');
+            }   
         }
         
     }
