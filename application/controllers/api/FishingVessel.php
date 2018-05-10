2951f2615d6e699fc88494e324577f9434869c61
Teerasej <teerasej@gmail.com>
2018-05-10 14:39:48 +0700

    สร้าง function รับ post เป็น json จาก mobile app และบันทึกลง database โดยส่งค่า true/false กลับไปให้ mobile app

--- application/controllers/api/FishingVessel.php
+++ application/controllers/api/FishingVessel.php
@@ -17,6 +17,21 @@ class FishingVessel extends CI_Controller {
 
         echo json_encode($data['vessels']);
     }
+    // /api/fishingvessel/create
+    public function create()
+    {
+        $input_data = json_decode(trim(file_get_contents('php://input')), true);
+
+        $vesselName = $input_data['Name'];
+        $countryID = $input_data['Country_ID'];
+
+        $this->load->model('fishingvessel_model');
+        $result = $this->fishingvessel_model->save_new_vessel_with_variable($vesselName, $countryID);
+
+        $data['result_info'] = $result;
+
+        echo json_encode($data);  
+    }
 
 }
 
